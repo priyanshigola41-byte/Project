824400d1837aad49a0521af8691577f59ed9d114
priyanshigola41 <priyanshigola41@gmail.com>
2026-08-01 14:26:29 +0530

    Change environment variable name to lowercase for Vercel compatibility

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001",
+  baseURL: import.meta.env.vite_api_url || "http://localhost:5001",
   withCredentials: true,
 });
